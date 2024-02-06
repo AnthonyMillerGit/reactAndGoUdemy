d729499c271a3259bad40bfe8976e3f80a047a6a
Anthony Miller <anthony@amiller.info>
2024-02-05 19:09:29 -0700

    add Form Skeleton

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Fragment, useState, useEffect } from "react";
+import "./App.css";
+function HelloWorld(props) {
+  const [isTrue, setIsTrue] = useState(false);
+  const [crowd, setCrowd] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dob, setDob] = useState("");
+
+  const toggleTrue = () => {
+    setIsTrue(!isTrue);
+  };
+
+  useEffect(() => {
+    console.log("useEffect fired!");
+
+    let people = [
+      { 
+        id: 1,
+        firstName: "Mary",
+        lastName: "Jones",
+        dob: "1997-05-02",
+      },
+      { 
+        id: 2,
+        firstName: "Jack",
+        lastName: "Smith",
+        dob: "1999-02-04",
+      },
+    ]
+    setCrowd(people)
+  }, []);
+
+  return (
+    <Fragment>
+      <hr />
+      <h1 className="h1-green">{props.msg}</h1>
+      <hr />
+      {isTrue ? <h1>True</h1> : <h1>False</h1>}
+      <hr />
+      <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>
+        Change True And False
+      </a>
+      <hr />
+        <form autoComplete="off">
+          <div className="mb-3">
+            <label className="form-label" htmlFor="first-name">First Name</label>
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              autoComplete="first-name-new"
+              className="form-control"
+              onChange={(e) => setFirstName(e.target.value)}
+            ></input>
+          </div>
+        </form>
+
+        <div>
+          First Name: {firstName}<br />
+          Last Name: {lastName}<br />
+          Date of Birth: {dob}<br />
+        </div>
+      <hr />
+      <h3>People</h3>
+      <ul className="list-group">
+        {crowd.map((m) => (
+          <li key={m.id} className="list-group-item">
+            {m.firstName} {m.lastName}
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default HelloWorld;
--- src/AppClass.js
+++ src/AppClass.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
-import './AppClass.css';
+import React, { Fragment, Component } from "react";
+import "./AppClass.css";
 export default class AppClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTrue: false,
+    };
+  }
+
+  toggleTrue = () => {
+    if (this.state.isTrue) {
+      this.setState({ isTrue: false });
+    } else {
+      this.setState({ isTrue: true });
+    }
+  };
   render() {
     return (
       <div>
-        <h1 className="h1-red">{this.props.msg}</h1>
+        <Fragment>
+          <hr />
+          <h1 className="h1-red">{this.props.msg}</h1>
+          <hr />
+          {this.state.isTrue ? <h1>True</h1> : <h1>False</h1>}
+          <hr />
+          <a
+            href="#!"
+            className="btn btn-outline-secondary"
+            onClick={this.toggleTrue}
+          >
+            Change True And False
+          </a>
+        </Fragment>
       </div>
     );
   }
